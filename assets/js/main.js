7a2709b6cf2425f87b6d81b2d27b39bb3a27c51f
Brian Done <briguy2078@gmail.com>
2021-11-03 18:51:40 -0400

    added event listeners for wrong and correct answers

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,43 +36,79 @@ h1El.className = "h1";
 quizEl.className = "quiz";
 buttonEl.className = "button";
 
-// BeginQuiz Function
-var beginQuiz = function () {
+// questionOne Function
+var questionOne = function () {
+
+    time = 75;
+    headerTimeEl.textContent = "Time: " + time + "";
+
     h1El.innerHTML = "<h2>Commonly used data types DO Not Include:</h2>";
     quizEl.innerHTML = "";
 
-    var q1a1El = document.createElement("button");
-    q1a1El.innerHTML = "1. Strings";
-    quizEl.appendChild(q1a1El);
-    q1a1El.className = "answer";
-
-    var space = document.createElement("div");
-    space.innerHTML ="";
-    quizEl.appendChild(space);
-
-    var q1a2El = document.createElement("button");
-    q1a2El.innerHTML = "2. Booleans";
-    quizEl.appendChild(q1a2El);
-    q1a2El.className = "answer";
-
-    var space = document.createElement("div");
-    space.innerHTML ="";
-    quizEl.appendChild(space);
-
-    var q1a3El = document.createElement("button");
-    q1a3El.innerHTML = "3. Alerts";
-    quizEl.appendChild(q1a3El);
-    q1a3El.className = "answer";
-
-    var space = document.createElement("div");
-    space.innerHTML ="";
-    quizEl.appendChild(space);
-
-    var q1a4El = document.createElement("button");
-    q1a4El.innerHTML = "4. Numbers";
-    quizEl.appendChild(q1a4El);
-    q1a4El.className = "answer";
+    var q1A1El = document.createElement("button");
+    q1A1El.innerHTML = "1. Strings";
+    quizEl.appendChild(q1A1El);
+    q1A1El.className = "answer";
     
+
+    var space = document.createElement("div");
+    space.innerHTML ="";
+    quizEl.appendChild(space);
+
+    var q1A2El = document.createElement("button");
+    q1A2El.innerHTML = "2. Booleans";
+    quizEl.appendChild(q1A2El);
+    q1A2El.className = "answer";
+    
+    var space = document.createElement("div");
+    space.innerHTML ="";
+    quizEl.appendChild(space);
+
+    var q1A3El = document.createElement("button");
+    q1A3El.innerHTML = "3. Alerts";
+    quizEl.appendChild(q1A3El);
+    q1A3El.className = "answer";
+    
+    
+    var space = document.createElement("div");
+    space.innerHTML ="";
+    quizEl.appendChild(space);
+
+    var q1A4El = document.createElement("button");
+    q1A4El.innerHTML = "4. Numbers";
+    quizEl.appendChild(q1A4El);
+    q1A4El.className = "answer";
+    
+    q1A1El.addEventListener("click", wrongAnswer);
+    q1A2El.addEventListener("click", wrongAnswer);
+    q1A3El.addEventListener("click", correctAnswer);
+    q1A4El.addEventListener("click", wrongAnswer);
+    
+
 };
 
-buttonEl.addEventListener("click", beginQuiz);
+// wrong alert function
+
+var wrongAnswer = function () {
+    time = time - 5;
+    headerTimeEl.textContent = "Time: " + time + "";
+    
+    var wrong = document.createElement("div");
+    // insert horizontal line
+    wrong.innerHTML = "Wrong!";
+    quizEl.appendChild(wrong);
+    wrong.className = "wrong";
+    // call next question
+}
+
+// correct alert function
+
+var correctAnswer = function () {
+    // insert horizontal line
+    correct.innerHTML = "Correct!";
+    quizEl.appendChild(correct);
+    correct.className = "correct";
+    // call next question
+}
+
+buttonEl.addEventListener("click", questionOne);
